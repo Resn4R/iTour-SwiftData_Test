4e27419876ed41256afc4e85cfd2e7a10a06e159
Resn4R <purview-joust.0x@icloud.com>
2023-10-01 18:00:18 +0100

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Vito Borghi on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
